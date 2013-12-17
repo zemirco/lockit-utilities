445331bca16ac8cc08ec699ff2724ae91c6e9c47
zeMirco <mirco.zeiss@gmail.com>
2013-12-17 17:01:26 +0100

    added debug statements

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+var debug = require('debug')('lockit-utils');
 // lockit-login attaches username and email to req.session
 
 // restrict routes to users who are logged in
@@ -13,11 +14,16 @@ exports.restrict = function(config) {
   // simple restrict function from express auth example
   return function(req, res, next) {
     if (req.session.username && req.session.email) {
+      debug('session found - username: %s, email: %s', req.session.username, req.session.email);
       next();
     } else {
+      debug('no session found -> redirecting to %s', route);
       // redirect to login page but save url the user really wanted to visit
       // lockit-login will make use of this
       req.session.redirectUrlAfterLogin = req.url;
+      // tell login route that user was redirected
+      req.session.restrictCalled = true;
+      // redirect user
       res.redirect(route);
     }
   };
