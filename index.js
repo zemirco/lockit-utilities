f01cfea5d5a3510ea6a2d7d1bff6f801e5221aad
zeMirco <mirco.zeiss@gmail.com>
2014-01-22 20:48:25 +0100

    new method getDatabase

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+var url = require('url');
 var debug = require('debug')('lockit-utils');
 // lockit-login attaches username and email to req.session
 
@@ -22,5 +23,42 @@ exports.restrict = function(config) {
       res.redirect(route + '?redirect=' + req.url);
     }
   };
+  
+};
+
+// get type of database from connection string
+// return an object with the type of database and the required adapter
+exports.getDatabase = function(config) {
+  
+  var urlObj = url.parse(config.db);  
+  var prot = urlObj.protocol;
+  
+  var res = {};
+    
+  switch (prot) {
+    case 'http:':
+    case 'https:':
+      res.type = 'couchdb';
+      res.adapter = 'lockit-couchdb-adapter';
+      break;
+    case 'mongodb:':
+      res.type = 'mongodb';
+      res.adapter = 'lockit-mongodb-adapter';
+      break;
+    case 'postgres:':
+      res.type = 'postgresql';
+      res.adapter = 'lockit-sql-adapter';
+      break;
+    case 'mysql:':
+      res.type = 'mysql';
+      res.adapter = 'lockit-sql-adapter';
+      break;
+    case 'sqlite:':
+      res.type = 'sqlite';
+      res.adapter = 'lockit-sql-adapter';
+      break;
+  }
+  
+  return res;
   
 };
