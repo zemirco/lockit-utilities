f4db0e1d9838034c7a1a426563dbd2b498e8a9d9
zeMirco <mirco.zeiss@gmail.com>
2014-05-06 18:08:36 +0200

    add docs and publish task

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,13 +23,16 @@ module.exports = function(grunt) {
       }
     },
     shell: {
-      docs: {
+      jsdoc: {
         command: [
           './node_modules/.bin/jsdoc index.js',
           '--configure ./docs/conf.json',
           '--destination console',
           '--template ./docs/templates/haruki/ > ./docs/docs.json'
         ].join(' ')
+      },
+      ghpages: {
+        command: 'git subtree push --prefix docs/ origin gh-pages'
       }
     },
     jade: {
@@ -37,7 +40,8 @@ module.exports = function(grunt) {
         options: {
           data: {
             title: '<%= pkg.name %>',
-            repo: '<%= pkg.repository.url %>'
+            repo: '<%= pkg.repository.url %>',
+            main: '<%= pkg.main %>'
           }
         },
         files: {
@@ -52,7 +56,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-contrib-handlebars');
 
   // write markdown files
   grunt.registerTask('gen', 'create markdown files from docs json', function() {
@@ -64,13 +67,25 @@ module.exports = function(grunt) {
   grunt.registerTask('index', 'create index.jade file', function() {
     // read handlebars template from source
     var source = fs.readFileSync('./docs/templates/index.handlebars.jade', 'utf8');
-    var template = handlebars.compile(source);
-    // get all markdown files from 'includes' folder
-    var files = glob.sync('includes/*.md', {
-      cwd: './docs/'
+
+    // handlebars helper for fn anchor tags, i.e. 'destroy (req, done)' -> 'destroy-req-done-'
+    handlebars.registerHelper('anchor', function(fn) {
+      var name = fn.name + '-';
+      var params = '';
+      fn.parameters.forEach(function(param) {
+        // check for params like config.key
+        if (param.name.indexOf('.') === -1) {
+          params += param.name + '-';
+        }
+      });
+      return (name + params).toLowerCase();
     });
+
+    var template = handlebars.compile(source);
     // write index.jade file
-    var data = template(files);
+    var data = template({
+      functions: require('./docs/docs.json').functions
+    });
     fs.writeFileSync('./docs/templates/index.jade', data);
   });
 
@@ -92,16 +107,14 @@ module.exports = function(grunt) {
     fs.writeFileSync('./README.md', data);
   });
 
-  grunt.registerTask('hint', ['jshint']);
-  grunt.registerTask('mocha', ['mochaTest']);
-
   grunt.registerTask('test', ['jshint', 'mochaTest']);
 
-  // 1. use jsdoc (grunt shell) to create ./docs/docs.json
+  // 1. use jsdoc (grunt shell:jsdoc) to create ./docs/docs.json
   // 2. use ./docs/generate.js to create ./docs/includes/*.md files
   // 3. use 'index' task to write 'index.jade' file that includes all .md files
   // 4. use 'jade' task to create 'index.html'
-  // 4. use 'readme' task to create 'README.md'
-  grunt.registerTask('docs', ['shell', 'gen', 'index', 'jade', 'readme']);
+  // 5. use 'readme' task to create 'README.md'
+  grunt.registerTask('docs', ['shell:jsdoc', 'gen', 'index', 'jade', 'readme']);
+  grunt.registerTask('publish', ['shell:ghpages']);
 
 };
