283bc33d6748b54f9bbfff8c0d00daa84b17705a
zeMirco <mirco.zeiss@gmail.com>
2014-05-28 21:58:18 +0200

    add pipe method

--- index.js
+++ index.js
@@ -189,3 +189,55 @@ exports.destroy = function(req, done) {
   done();
 
 };
+
+
+
+/**
+ * Pipe events from `source` to `target`. `source` can be a single event
+ * emitter or an Array of event emitters.
+ *
+ * @example
+   var util = require('util');
+   var events = require('events');
+   var utils = require('lockit-utils');
+
+   var Child = function() {};
+   util.inherits(Child, events.EventEmitter);
+
+   var Mother = function() {};
+   util.inherits(Mother, events.EventEmitter);
+
+   var child = new Child();
+   var mother = new Mother();
+
+   utils.pipe(child, mother);
+
+   mother.on('action', function(action) {
+     console.log('look the child is ' + action);
+   });
+
+   child.emit('action', 'smiling');
+ *
+ * @param {Object|Array} source - Single event emitter or Array of event emitters
+ * @param {Object} target - Single event emitter
+ */
+exports.pipe = function(source, target) {
+
+  var isArray = Array.isArray(source);
+
+  if (!isArray) return pipe(source, target);
+
+  source.forEach(function(event) {
+    pipe(event, target);
+  });
+
+  function pipe(source, target) {
+    var emit = source.emit;
+    source.emit = function () {
+      emit.apply(source, arguments);
+      target.emit.apply(target, arguments);
+      return source;
+    };
+  }
+
+};
